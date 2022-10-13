d00802e8fe590cf3acaa1569336e2c51565e083c
OTIKLO <aaa7284@naver.com>
2022-10-13 16:41:58 +0900

    배경그라데이션

--- screens/Start.js
+++ screens/Start.js
@@ -3,43 +3,41 @@ import { View, StyleSheet, Image, TouchableOpacity } from "react-native";
 import { theme } from "../Color";
 import kakao from "../assets/images/kakaologin.png";
 import logo from "../assets/images/logo.png";
+import { LinearGradient } from 'expo-linear-gradient'
 
-function Start({navigation}) {
-    return (
-        <View style={styles.container}>
-            <View style={styles.header}></View>
-            <View style={styles.content}>
-                <Image source={logo} />
-            </View>
-            <View style={styles.footer}>
-                <TouchableOpacity onPress={() => navigation.navigate('Myinfo_insert')}>
-                    <Image source={kakao}/>
-                </TouchableOpacity>
-            </View>
-        </View>
-    );
+function Start({ navigation }) {
+  return (
+    <LinearGradient style={styles.container} colors={['#36622B', '#729D39','#C6E377','white']}>
+      <View style={styles.header}></View>
+      <View style={styles.content}>
+        <Image source={logo} />
+      </View>
+      <View style={styles.footer}>
+        <TouchableOpacity onPress={() => navigation.navigate('Myinfo_insert')}>
+          <Image source={kakao} />
+        </TouchableOpacity>
+      </View>
+    </LinearGradient>
+  );
 }
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-    },
-    header: {
-      height: 60,
-      backgroundColor: theme.light_green,
-    },
-    content: {
-      flex: 1,
-      backgroundColor: '#ffffff',
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    footer: {
-      height: 300,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#ffff',
-    },
-  });
+  container: {
+    flex: 1,
+  },
+  header: {
+    height: 60,
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  footer: {
+    height: 200,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 /*
 const styles = StyleSheet.create({
     container: {
