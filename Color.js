8cb230dc1f3c97411519f27bc01abe57eb75a91f
superminsu <alstn6870@gmail.com>
2022-10-26 20:34:12 +0900

    일단 저장

--- Color.js
+++ Color.js
@@ -4,4 +4,7 @@ export const theme = {
     green: "#729D39",
     dark_green: "#36622B",
     //파스텔: FEFFDE DDFFBC 91C788 52734D
+    bg: "black",
+    grey: "#3A3D40",
+    toDoBg: "#1A1C20",
 }
--- screens/Staff_managments.js
+++ screens/Staff_managments.js
@@ -1,99 +1,139 @@
-import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity, Image, ScrollView, TextInput } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View, TouchableOpacity, TextInput, ScrollView, Image } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { theme } from "../Color";
 import user from '../assets/images/icon/user.png';
 
+const STORAGE_KEY = "@toDos";
+
 function Staff_managments({ navigation }) {
     const arrow = ">";
+    const [working, setWorking] = useState(true);
+    const [text, setText] = useState("");
+    const [toDos, setToDos] = useState({});
+    useEffect(() => {
+        loadToDos();
+    }, []);
+    const travel = () => setWorking(false);
+    const work = () => setWorking(true);
+    const onChangeText = (payload) => setText(payload);
+    const saveToDos = async (toSave) => {
+        await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(toSave));
+    };
+    const loadToDos = async () => {
+        const s = await AsyncStorage.getItem(STORAGE_KEY);
+        setToDos(JSON.parse(s));
+    };
+
+    const addToDo = async () => {
+        if (text === "") {
+            return;
+        }
+        const newToDos = {
+            ...toDos,
+            [Date.now()]: { text, working },
+        };
+        setToDos(newToDos);
+        await saveToDos(newToDos);
+        setText("");
+    };
     return (
         <View style={styles.container}>
+            <StatusBar style="auto" />
             <View style={styles.header}>
-                <TouchableOpacity>
-                    <Text style={styles.headerText}>직원 관리</Text>
+                <TouchableOpacity onPress={work}>
+                    <Text
+                        style={{ ...styles.btnText, color: working ? "green" : theme.grey }}
+                    >
+                        직원관리
+                    </Text>
                 </TouchableOpacity>
-                <TouchableOpacity>
-                    <Text style={styles.headerText}>직원 현황</Text>
+                <TouchableOpacity onPress={travel}>
+                    <Text
+                        style={{
+                            ...styles.btnText,
+                            marginRight: 30,
+                            color: !working ? "green" : theme.grey,
+                        }}
+                    >
+                        근무현황
+                    </Text>
                 </TouchableOpacity>
             </View>
-            <View style={styles.body}>
-                <View style={styles.body_top}>
-                    <View style={{margin: 10, borderLeftWidth: 2, borderColor: theme.green}}><Text></Text></View>
-                    <Text style={styles.shopname}>맥도날드 송도 GSD</Text>
+            <ScrollView style={styles.notice}>
+                <View style={styles.notice_body}>
+                    <TouchableOpacity onPress={() => navigation.navigate('Staff')}>
+                        <View style={styles.satffList}>
+                            <Image source={user} style={styles.img} />
+                            <Text style={{ fontSize: 20, marginRight: 250 }}>장민수</Text>
+                            <Text style={{ fontSize: 30 }}>{arrow}</Text>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <View style={styles.satffList}>
+                            <Image source={user} style={styles.img} />
+                            <Text style={{ fontSize: 20, marginRight: 250 }}>황의충</Text>
+                            <Text style={{ fontSize: 30 }}>{arrow}</Text>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <View style={styles.satffList}>
+                            <Image source={user} style={styles.img} />
+                            <Text style={{ fontSize: 20, marginRight: 250 }}>홍길동</Text>
+                            <Text style={{ fontSize: 30 }}>{arrow}</Text>
+                        </View>
+                    </TouchableOpacity>
                 </View>
-                <ScrollView style={styles.notice}>
-                    <View style={styles.notice_body}>
-                        <TouchableOpacity onPress={() => navigation.navigate('Staff')}>
-                            <View style={styles.satffList}>
-                                <Image source={user} style={styles.img}/>
-                                <Text style={{fontSize:20, marginRight:250}}>장민수</Text>
-                                <Text style={{fontSize: 30}}>{arrow}</Text>
-                            </View>
-                        </TouchableOpacity>
-                        <TouchableOpacity>
-                            <View style={styles.satffList}>
-                                <Image source={user} style={styles.img}/>
-                                <Text style={{fontSize:20, marginRight:250}}>황의충</Text>
-                                <Text style={{fontSize: 30}}>{arrow}</Text>
-                            </View>
-                        </TouchableOpacity>
-                        <TouchableOpacity>
-                            <View style={styles.satffList}>
-                                <Image source={user} style={styles.img}/>
-                                <Text style={{fontSize:20, marginRight:250}}>홍길동</Text>
-                                <Text style={{fontSize: 30}}>{arrow}</Text>
-                            </View>
-                        </TouchableOpacity>
+                <View style={styles.schedule_top}>
+                    <Text style={{ fontSize: 25, }}>근무 스케줄</Text>
+                    <TouchableOpacity>
+                        <Text style={{ fontSize: 15, marginTop: 10 }}>모두 보기</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.date_select}>
+                    <TextInput style={styles.scheduleDate}>    2022년      ▼</TextInput>
+                    <TextInput style={styles.scheduleDate}>      10월      ▼</TextInput>
+                    <TextInput style={styles.scheduleDate}>       9일      ▼</TextInput>
+                </View>
+                <View style={styles.scheduleList}>
+                    <Image source={user} style={styles.img} />
+                    <View style={{ width: 220, marginLeft: 10 }}>
+                        <Text style={{ fontWeight: "600", fontSize: 17 }}>10월 9일</Text>
+                        <View style={{ flexDirection: 'row' }}>
+                            <Text>10 : 00 AM ~ 16 : 00 PM</Text>
+                        </View>
+                        <Text>근무자 : 황의충</Text>
                     </View>
-                    <View style={styles.schedule_top}>
-                        <Text style={{fontSize: 25,}}>근무 스케줄</Text>
-                        <TouchableOpacity>
-                            <Text style={{fontSize: 15, marginTop: 10}}>모두 보기</Text>
-                        </TouchableOpacity>
+                </View>
+                <View style={styles.scheduleList}>
+                    <Image source={user} style={styles.img} />
+                    <View style={{ width: 220, marginLeft: 10 }}>
+                        <Text style={{ fontWeight: "600", fontSize: 17 }}>10월 9일</Text>
+                        <View style={{ flexDirection: 'row' }}>
+                            <Text>16 : 00 PM ~ 22 : 00 AM</Text>
+                        </View>
+                        <Text>근무자 : 장민수</Text>
                     </View>
-                    <View style={styles.date_select}>
-                        <TextInput style={styles.scheduleDate}>    2022년      ▼</TextInput>
-                        <TextInput style={styles.scheduleDate}>      10월      ▼</TextInput>
-                        <TextInput style={styles.scheduleDate}>       9일      ▼</TextInput>
-                    </View>
-                    <View style={styles.scheduleList}>
-                            <Image source={user} style={styles.img}/>
-                            <View style={{width:220, marginLeft: 10}}>
-                                <Text style={{ fontWeight: "600", fontSize: 17 }}>10월 9일</Text>
-                                <View style={{ flexDirection: 'row' }}>
-                                    <Text>10 : 00 AM ~ 16 : 00 PM</Text>
-                                </View>
-                                <Text>근무자 : 황의충</Text>
-                            </View>
-                    </View>
-                    <View style={styles.scheduleList}>
-                            <Image source={user} style={styles.img}/>
-                            <View style={{width:220,  marginLeft: 10}}>
-                                <Text style={{ fontWeight: "600", fontSize: 17 }}>10월 9일</Text>
-                                <View style={{ flexDirection: 'row' }}>
-                                    <Text>16 : 00 PM ~ 22 : 00 AM</Text>
-                                </View>
-                                <Text>근무자 : 장민수</Text>
-                            </View>
-                    </View>
-                </ScrollView>
-            </View>
+                </View>
+            </ScrollView>
         </View>
     );
 }
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor:'white'
+        backgroundColor: 'white'
     },
     header: {
         justifyContent: "space-between",
         flexDirection: "row",
         height: 80,
         backgroundColor: 'white',
-        borderBottomWidth:1,
+        borderBottomWidth: 1,
         borderBottomColor: '#cccccc',
     },
-    headerText: {
+    btnText: {
         marginLeft: 30,
         marginTop: 40,
         fontSize: 20,
@@ -115,9 +155,9 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
     },
     shopname: {
-        fontSize:20,
-        fontWeight:"600",
-        marginTop:5,
+        fontSize: 20,
+        fontWeight: "600",
+        marginTop: 5,
     },
     scheduleDate: {
         height: 50,
@@ -132,10 +172,10 @@ const styles = StyleSheet.create({
         textAlign: 'center'
     },
     notice: {
-        backgroundColor:'#F4FAE4',
+        backgroundColor: '#F4FAE4',
         height: '100%'
     },
-    notice_body:{
+    notice_body: {
         borderTopWidth: 1,
         borderBottomWidth: 1,
         borderColor: theme.light_green,
@@ -144,7 +184,7 @@ const styles = StyleSheet.create({
     satffList: {
         flexDirection: 'row',
         justifyContent: 'space-between',
-        alignItems:'center',
+        alignItems: 'center',
         padding: 10,
         paddingBottom: 20,
         marginTop: 15,
@@ -173,6 +213,4 @@ const styles = StyleSheet.create({
         backgroundColor: 'white'
     },
 });
-
-
 export default Staff_managments;
